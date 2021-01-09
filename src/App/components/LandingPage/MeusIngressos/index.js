178ffc2a72e9e7c5d0b91b430974f07626b416b6
Enrique Bonifacio <enriqueboni80@gmail.com>
2021-01-09 19:08:30 -0300

    Insere mensagem de usuario ainda nao tem nenhum curso cadastrado

--- src/App/components/LandingPage/MeusIngressos/index.js
+++ src/App/components/LandingPage/MeusIngressos/index.js
@@ -53,40 +53,51 @@ class Index extends React.Component {
         return (
             <Aux>
                 <Row>
-                    {this.state.eventos.map((evento) => {
-                        if (evento.publicado) {
-                            return (
-                                <Col md={6} xl={3} key={evento.id}>
-                                    <Card>
-                                        <Card.Body>
-                                            <Card.Img src={evento.url_imagem ? evento.url_imagem : `${process.env.REACT_APP_FRONTEND_SERVER_URL}/images/evento-padrao-img.png`} />
-                                            <h5 className="m-t-35">{evento.name}</h5>
-                                            <span className="text-muted d-block m-b-30">{evento.detalhes}</span>
-                                            <p className="border-top m-b-20 p-t-10 m-t-20"></p>
-                                            <div className="row" style={{ textAlign: "center" }}>
-                                                <div className="col">
-                                                    <h5>{evento.gratuito ? 'GRATUITO' : evento.preco ? 'R$ ' + convertCurrencyUStoPT(evento.preco) : ''}</h5>
-                                                    <span>Investimento</span>
+                    {this.state.eventos.length <= 0
+                        ?
+                        <div class="col-sm-12">
+                            <div class="alert alert-primary" role="alert">
+                                <p style={{textAlign: 'center'}}>Você ainda não esta inscrito em nenhum evento - <a href="/" class="alert-link">HOME</a></p><br />
+                                <label class="text-muted">O Portal de Eventos Acadêmicos PUC Minas objetiva ser espaço para que as diversas atividades
+                                acadêmicas promovidas na universidade - Simpósios, Congressos, Seminários, Encontros, Semanas ou Jornadas Acadêmicas,
+                                Conferências, Palestras e demais atividades.</label>
+                            </div>
+                        </div>
+                        :
+                        this.state.eventos.map((evento) => {
+                            if (evento.publicado) {
+                                return (
+                                    <Col md={6} xl={3} key={evento.id}>
+                                        <Card>
+                                            <Card.Body>
+                                                <Card.Img src={evento.url_imagem ? evento.url_imagem : `${process.env.REACT_APP_FRONTEND_SERVER_URL}/images/evento-padrao-img.png`} />
+                                                <h5 className="m-t-35">{evento.name}</h5>
+                                                <span className="text-muted d-block m-b-30">{evento.detalhes}</span>
+                                                <p className="border-top m-b-20 p-t-10 m-t-20"></p>
+                                                <div className="row" style={{ textAlign: "center" }}>
+                                                    <div className="col">
+                                                        <h5>{evento.gratuito ? 'GRATUITO' : evento.preco ? 'R$ ' + convertCurrencyUStoPT(evento.preco) : ''}</h5>
+                                                        <span>Investimento</span>
+                                                    </div>
+                                                    <div className="col">
+                                                        <h5>{convertDateUStoPT(evento.prazo_inscricao)}</h5>
+                                                        <span>Prazo Inscrição</span>
+                                                    </div>
                                                 </div>
-                                                <div className="col">
-                                                    <h5>{convertDateUStoPT(evento.prazo_inscricao)}</h5>
-                                                    <span>Prazo Inscrição</span>
+                                                <div className="row m-t-30" style={{ margin: "30px auto 1px" }}>
+                                                    <div className="col-6 p-r-0">
+                                                        {this.state.isAuthenticated ? <a href='#' className="btn btn-danger text-uppercase btn-block" onClick={(e) => this.desinscricaoEvento(e, evento.evento_id)}>desinscrever</a> : ""}
+                                                    </div>
+                                                    <div className="col-6">
+                                                        <Link to={`../evento/${evento.id}`} className="btn text-uppercase border btn-block btn-outline-secondary">Ver Detalhes</Link>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <div className="row m-t-30" style={{ margin: "30px auto 1px" }}>
-                                                <div className="col-6 p-r-0">
-                                                    {this.state.isAuthenticated ? <a href='#' className="btn btn-danger text-uppercase btn-block" onClick={(e) => this.desinscricaoEvento(e, evento.evento_id)}>desinscrever</a> : ""}
-                                                </div>
-                                                <div className="col-6">
-                                                    <Link to={`../evento/${evento.id}`} className="btn text-uppercase border btn-block btn-outline-secondary">Ver Detalhes</Link>
-                                                </div>
-                                            </div>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                            )
-                        }
-                    })}
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+                                )
+                            }
+                        })}
                 </Row>
             </Aux>
         );
