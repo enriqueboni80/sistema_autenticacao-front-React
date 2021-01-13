bc18555ed11d096302647253b0eb7f9c64892330
Enrique Bonifacio <enriqueboni80@gmail.com>
2021-01-13 13:55:00 -0300

    Ajusta Front Meus Ingressos

--- src/App/components/LandingPage/MeusIngressos/index.js
+++ src/App/components/LandingPage/MeusIngressos/index.js
@@ -3,11 +3,10 @@ import { Row, Col, Card, Table, Modal, Button } from 'react-bootstrap';
 import Service from "../../../../services/InscricaoService"
 import Aux from "../../../../hoc/_Aux";
 import { convertCurrencyUStoPT } from "../../../../helpers/convertCurrency"
-import { convertDateUStoPT } from "../../../../helpers/convertDate"
+import { convertDateUStoPT, getTimeSplited } from "../../../../helpers/convertDate"
 import InscricaoService from "./../../../../services/InscricaoService"
 
 import { Link } from 'react-router-dom'
-
 
 
 class Index extends React.Component {
@@ -77,29 +76,29 @@ class Index extends React.Component {
                         this.state.eventos.map((evento) => {
                             if (evento.publicado) {
                                 return (
-                                    <Col md={6} xl={3} key={evento.id}>
+                                    <Col md={4} xl={3} key={evento.id}>
                                         <Card>
                                             <Card.Body>
                                                 <Card.Img src={evento.url_imagem ? evento.url_imagem : `${process.env.REACT_APP_FRONTEND_SERVER_URL}/images/evento-padrao-img.png`} />
                                                 <h5 className="m-t-35">{evento.name}</h5>
                                                 <span className="text-muted d-block m-b-30">{evento.detalhes}</span>
                                                 <p className="border-top m-b-20 p-t-10 m-t-20"></p>
-                                                <div className="row" style={{ textAlign: "center" }}>
+                                                <div className="row">
                                                     <div className="col">
-                                                        <h5>{evento.gratuito ? 'GRATUITO' : evento.preco ? 'R$ ' + convertCurrencyUStoPT(evento.preco) : ''}</h5>
-                                                        <span>Investimento</span>
-                                                    </div>
-                                                    <div className="col">
-                                                        <h5>{convertDateUStoPT(evento.prazo_inscricao)}</h5>
-                                                        <span>Prazo Inscrição</span>
+                                                        <h5><b>Data:</b> {convertDateUStoPT(evento.data_inicio)} as {getTimeSplited(evento.data_inicio)}</h5>
                                                     </div>
                                                 </div>
-                                                <div className="row m-t-30" style={{ margin: "30px auto 1px" }}>
-                                                    <div className="col-6 p-r-0">
-                                                        {this.state.isAuthenticated ? <a href='#' className="btn btn-danger text-uppercase btn-block" onClick={(e) => this.desinscricaoEvento(e, evento.evento_id)}>desinscrever</a> : ""}
+                                                <div className="row">
+                                                    <div className="col">
+                                                        <h5><b>Investimento:</b> {evento.gratuito ? 'GRATUITO' : evento.preco ? 'R$ ' + convertCurrencyUStoPT(evento.preco) : ''}</h5>
+                                                    </div>
+                                                </div>
+                                                <div className="row m-t-30" style={{ marginTop: "30px auto 1px" }}>
+                                                    <div className="col-6">
+                                                        {this.state.isAuthenticated ? <a href='#' className="btn btn-danger text-uppercase btn-block" onClick={(e) => this.desinscricaoEvento(e, evento.evento_id)}>cancelar</a> : ""}
                                                     </div>
                                                     <div className="col-6">
-                                                        <Link to={`../evento/${evento.id}`} className="btn text-uppercase border btn-block btn-outline-secondary">Ver Detalhes</Link>
+                                                        <Link to={`../evento/${evento.id}`} className="btn text-uppercase border btn-block btn-outline-secondary">+ detalhes</Link>
                                                     </div>
                                                 </div>
                                             </Card.Body>
